99285c106688250ee444085311cbe1790b99db38
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2020-02-10 23:00:27 +0600

    Update SOLUTION_TEMP.cpp

--- SOLUTION_TEMP.cpp
+++ SOLUTION_TEMP.cpp
@@ -6,20 +6,25 @@ typedef pair<ll,ll> ii;
 typedef pair<ii,ll> iii;
 typedef vector<ll> vi;
 typedef vector<ii> vii;
-#define getbit(n,i) (((n)&(1<<(i)))!=0)
-#define setbit0(n,i) ((n)&(~(1<<(i))))
-#define setbit1(n,i) ((n)|(1<<(i)))
-#define togglebit(n,i) ((n)^(1<<(i)))
+#define getbit(n,i) (((n)&(1LL<<(i)))!=0)
+#define setbit0(n,i) ((n)&(~(1LL<<(i))))
+#define setbit1(n,i) ((n)|(1LL<<(i)))
 #define lastone(n) ((n)&(-(n)))
 #define read freopen("debug\\in.txt","r",stdin)
 #define write freopen("debug\\out.txt","w",stdout)
+#define DBG(a) cout<<"a# ->->->-> "<<a<<"\n"
+#define EN cout<<"\n"
 #define fi first
 #define se second
 #define PI (acos(-1))
-#define M_E (2.718281828459045)
 #define fastread ios::sync_with_stdio(false);cin.tie(NULL)
 #define mod (1000000007)
 #define asz 500005
+template<class T> ostream& operator<<(ostream &s,pair<T,ll> a)
+{
+    s<<a.fi<<' '<<a.se;
+    return s;
+}
 int main()
 {
     fastread;
