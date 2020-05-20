253073888315e2d751643f143b9596a27ce1d7e6
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2020-05-20 13:36:08 +0600

    Update segment_tree.cpp

--- DS/segment_tree.cpp
+++ DS/segment_tree.cpp
@@ -38,3 +38,25 @@ ll query(ll node,ll ss,ll sf,ll qs,ll qe)
     return f(query(2*node,ss,mid,qs,qe),query(2*node+1,mid+1,sf,qs,qe));
 }
 
+//Modified update for ordered multiset
+int update(ll node,ll ss,ll sf,ll pos,int val)    //returns i(pos)-th smallest element if val==0. if val>0, then adds pos val times. 
+{                                                 //Otherwise deletes i(pos)-th smallest element val times.
+    if(ss==sf)                                    //to find number of smaller element than x, use query(0,x-1)
+    {
+        st[node]+=val;
+        if(st[node]<0)st[node]=0;
+        return ss;
+    }
+    ll mid=(ss+sf)/2;
+    int x=pos;
+    if(val>0){
+        if(pos<=mid)update(2*node,ss,mid,pos,val);
+        else update(2*node+1,mid+1,sf,pos,val);
+    }
+    else{
+        if(st[2*node]>=pos)x=update(2*node,ss,mid,pos,val);
+        else x=update(2*node+1,mid+1,sf,pos-st[2*node],val);
+    }
+    st[node]=f(st[2*node],st[2*node+1]);
+    return x;
+}
