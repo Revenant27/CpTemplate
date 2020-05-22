dc483b14b7619c66180c64fea5222e751f42aeb9
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2020-05-22 21:46:01 +0600

    Create Gaussian elimination.cpp

---
+++ Math/Gaussian elimination.cpp
@@ -0,0 +1,27 @@
+int gauss(vector<vector<double>> &mat,vector<double> &ans){        //returns -1 if impossible,1 if solution is unique, 0 otherwise
+    int n=mat.size(),m=mat[0].size()-1;             
+    vector<int> w(m,-1);
+    int ret=1;
+    for(int i=0,j=0;j<n&&i<m;i++){
+        int mx=j;
+        for(int k=j+1;k<n;k++)if(mat[mx][i]<mat[k][i])mn=k;
+        if(abs(mat[mx][i])<EPS){
+            ret=0;
+            continue;
+        }
+        for(int k=i;k<=m;k++)swap(mat[j][k],mat[mx][k]);
+        for(int k=0;k<n;k++){
+            double d=mat[k][i]/mat[j][i];
+            for(int l=j;j<=m;j++)mat[k][l]-=d*mat[j][l];
+        }
+        w[i]=j++;
+    }
+    ans.assign(m,0);
+    for(int i=0;i<m;i++)if(w[i]!=-1)ans[i]=mat[w[i]][m]/mat[w[i]][i];
+    for(int i=0;i<n;i++){
+        double d=0;
+        for(int j=0;j<m;j++)d+=ans[j]*mat[i][j];
+        if(abs(d-mat[i][m])>EPS)return -1;
+    }
+    return ret;
+}
