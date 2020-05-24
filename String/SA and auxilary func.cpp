9bb58baa1b79872ddc6f307d5d5e3ad4c327a2de
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2020-05-24 12:53:59 +0600

    Update SA and auxilary func.cpp

--- String/SA and auxilary func.cpp
+++ String/SA and auxilary func.cpp
@@ -104,3 +104,71 @@ void compute_lcp(string &s)
     for(int i=0;i<s.size();i++)LCP[i]=PLCP[sa[i]];
     s.pop_back();
 }
+
+
+
+
+
+//SA using DA
+using namespace SA{
+    const int N=1e5+5;
+    const int log_N=20;
+    int sa[N],ra[N],rnk[N],hg[N],n;
+    int wa[N],wb[N],wws[N],wv[N];
+    int lg[N], st[N][LOG];
+    int cmp(int *r,int a,int b,int l){
+        return (r[a]==r[b]) && (r[a+l]==r[b+l]);
+    }
+    void DA(int *r,int *sa,int n,int m){
+        int i,j,p,*x=wa,*y=wb,*t;
+        for(i=0;i<m;i++) wws[i]=0;
+        for(i=0;i<n;i++) wws[x[i]=r[i]]++;
+        for(i=1;i<m;i++) wws[i]+=wws[i-1];
+        for(i=n-1;i>=0;i--) sa[--wws[x[i]]]=i;
+        for(j=1,p=1;p<n;j*=2,m=p) {
+            for(p=0,i=n-j;i<n;i++) y[p++]=i;
+            for(i=0;i<n;i++) if(sa[i]>=j) y[p++]=sa[i]-j;
+            for(i=0;i<n;i++) wv[i]=x[y[i]];
+            for(i=0;i<m;i++) wws[i]=0;
+            for(i=0;i<n;i++) wws[wv[i]]++;
+            for(i=1;i<m;i++) wws[i]+=wws[i-1];
+            for(i=n-1;i>=0;i--) sa[--wws[wv[i]]]=y[i];
+            for(t=x,x=y,y=t,p=1,x[sa[0]]=0,i=1;i<n;i++)
+                x[sa[i]]=cmp(y,sa[i-1],sa[i],j)?p-1:p++;
+        }
+    }
+    void create_sa(string  &s) {
+        n = s.size();
+        for(int i=0;i<max(n+1,ALPHA);i++) sa[i]=ra[i]=rnk[i]=hg[i]=wa[i]=wb[i]=wws[i]=wv[i]=0;
+        for (int i = 0; i < n; i++) ra[i] = s[i];
+        DA(ra,sa,n+1,ALPHA);
+        calheight(ra,sa,n);
+        init();
+    }
+    void calheight(int *r,int *sa,int n){
+        int i,j,k=0;
+        for(i=1;i<=n;i++) rnk[sa[i]]=i;
+        for(i=0;i<n;hg[rnk[i++]]=k)
+            for(k?k--:0,j=sa[rnk[i]-1];r[i+k]==r[j+k];k++);
+    }
+    void init() {
+        for(int i = 0; i <=n; i++) st[i][0] = hg[i];
+        for(int j = 1; j < log_n; j++) {
+            for(int i = 0; i <=n; i++) {
+                if (i+(1<<j)-1 <= n) st[i][j] = min(st[i][j-1],st[i+(1<<(j-1))][j-1]);
+                else break;
+            }
+        }
+        lg[0] = lg[1] = 0;
+        for(int i = 2; i <= n; i++) {
+            lg[i] = lg[i/2] + 1;
+        }
+    }
+    //1 indexed
+    int query(int l, int r) {
+        if(l == r) return n-sa[l];
+        l++;
+        int k = lg[r-l+1];
+        return st(rmq[l][k],st[r-(1<<k)+1][k]);
+    }
+}
