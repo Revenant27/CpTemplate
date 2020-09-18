7c73e83679db89690ff63ce16905e23eafa512ae
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2020-09-18 13:04:21 +0600

    Update Tree_isomorphism.cpp

--- Graph/Tree_isomorphism.cpp
+++ Graph/Tree_isomorphism.cpp
@@ -6,14 +6,14 @@ struct Tree_iso
     vector<int> val[2];
     vi dg[2];
     int n,rt[2];
-    Tree_iso(int _n,int r1=1,int r2=1):n(_n)
+    Tree_iso(int _n,int r1=0,int r2=0):n(_n)
     {
         rt[0]=r1-1,rt[1]=r2-1;
         for(int i=0; i<2; i++)
         {
             adj[i].resize(n);
-            lv[i].resize(n);
-            dg[i].resize(n);
+            lv[i].assign(n,vi());
+            dg[i].assign(n,0);
             par[i].assign(n,-1);
             val[i].assign(n,0);
         }
@@ -29,13 +29,15 @@ struct Tree_iso
         lv[t][d].push_back(ind);
         par[t][ind]=p;
         for(auto x:adj[t][ind])
-            if(x!=p){
+            if(x!=p)
+            {
                 dfs(x,t,ind,d+1);
                 dg[t][ind]++;
             }
     }
     //O(nlogn)
-    bool Iso_2(){
+    bool Iso_2_util()
+    {
         dfs(rt[0],0);
         dfs(rt[1],1);
         for(int i=n-1; i>=0; i--)
@@ -59,10 +61,12 @@ struct Tree_iso
             sort(v.begin(),v.end());
             int l=0;
             val[v[0].se.se][v[0].se.fi]=0;
-            int a[2]={0};
+            int a[2]= {0};
             a[v[0].se.se]++;
-            for(int j=1;j<v.size();j++){
-                if(v[j].fi!=v[j-1].fi){
+            for(int j=1; j<v.size(); j++)
+            {
+                if(v[j].fi!=v[j-1].fi)
+                {
                     if(a[0]!=a[1])return false;
                     a[0]=a[1]=0;
                     l++;
@@ -76,7 +80,7 @@ struct Tree_iso
     }
 
     //O(n)
-    bool Iso()
+    bool Iso_util()
     {
         dfs(rt[0],0);
         dfs(rt[1],1);
@@ -164,5 +168,53 @@ struct Tree_iso
             }
         }
         return true;
+    }
+    ii dfs2(int ind,vi &pr,int t)
+    {
+        ii mx=ii(-1,ind);
+        for(auto x:adj[t][ind])if(pr[ind]!=x)pr[x]=ind,mx=max(mx,dfs2(x,pr,t));
+        mx.fi++;
+        return mx;
+    }
+    void restor(){
+        for(int i=0; i<2; i++)
+        {
+            lv[i].assign(n,vi());
+            dg[i].assign(n,0);
+            par[i].assign(n,-1);
+            val[i].assign(n,0);
+        }
+    }
+    bool ISO(int t=0)
+    {
+        if(rt[0]==-1||rt[1]==-1)
+        {
+            vi pr(n,-1);
+            ii a=dfs2(0,pr,0);
+            pr[a.se]=-1;
+            ii b=dfs2(a.se,pr,0);
+            vi cn,cn1;
+            while(b.se!=-1)cn.push_back(b.se),b.se=pr[b.se];
+            pr[0]=-1;
+            a=dfs2(0,pr,1);
+            pr[a.se]=-1;
+            b=dfs2(a.se,pr,1);
+            while(b.se!=-1)cn1.push_back(b.se),b.se=pr[b.se];
+            if(cn.size()%2!=cn1.size()%2)return false;
+            if(cn.size()%2==0)
+            {
+                rt[0]=cn[cn.size()/2];
+                rt[1]=cn1[cn1.size()/2-1];
+                if(t&&Iso_2_util())return true;
+                else if(!t&&Iso_util())return true;
+                restor();
+
+            }
+            rt[0]=cn[cn.size()/2];
+            rt[1]=cn1[cn1.size()/2];
+        }
+        if(t&&Iso_2_util())return true;
+        else if(!t&&Iso_util())return true;
+        return false;
     }
 };
