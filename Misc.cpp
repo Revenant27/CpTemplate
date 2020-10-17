b73601fda4afcfe366abb282900a846f93c238bb
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2020-10-17 16:29:27 +0600

    Update Misc.cpp

--- Misc.cpp
+++ Misc.cpp
@@ -1,1 +1,6 @@
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+
+//factorial inverse (Linear)
+invf[0]=invf[1]=1;
+for(int i=2;i<asz;i++)invf[i]=(mod-(mod/i)*invf[mod%i]%mod)%mod;
