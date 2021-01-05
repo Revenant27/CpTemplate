8456964cf354d95616b431a5078d26d8f2d6c563
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2021-01-05 20:04:31 +0600

    Update bitwise_sieve.cpp

--- Math/bitwise_sieve.cpp
+++ Math/bitwise_sieve.cpp
@@ -30,3 +30,49 @@ void sieve()
     }
 }
 
+
+
+
+
+//4 sec prime genarator
+
+#define asz 1000000001
+#define bbb 15625001
+int bs[bbb];
+bool isprime(ll x)
+{
+    return !(bs[x/64] & (1 << ((x >> 1) & 31)));
+}
+void make_comp(ll x)
+{
+    bs[x/64] |= (1 << ((x >> 1) & 31));
+}
+ll skip[210 ];
+int prime[60000005];
+int cnt=0;
+void sieve()
+{
+
+    ll incr[]={10,2,4,2,4,6,2,6,4,2,4,6,6,2,6,4,2,6,4,6,8,4,2,4,2,4,8,6,4,6,2,4,6,2,6,6,4,2,4,6,2,6,4,2,4,2,10,2};
+    prime[cnt++] = 2;
+    prime[cnt++] = 3;
+    prime[cnt++] = 5;
+    prime[cnt++] = 7;
+    ll u=1;
+    for(ll i=11;i<asz;i+=incr[u++])
+    {
+        if(isprime(i)){
+        if((long long)i*i<asz)
+        {
+            ll k=skip[i%210];
+            for(ll j=i*i;j<asz;j+=incr[k++]*i)
+            {
+                make_comp(j);
+                if(k==48)k=0;
+            }
+        }
+        prime[cnt++] = i;
+        }
+        if(u==48)u=0;
+    }
+}
