4a0a574f2ea739a010d05abdddcacb2b38ac5d8b
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2021-03-17 13:17:44 +0600

    Update Treap.cpp

--- DS/Treap.cpp
+++ DS/Treap.cpp
@@ -173,6 +173,168 @@ public:
 
 //MAY B IT"S BETTER
 
+//Indexed from 1
+int ara[N];
+unordered_map<ll,ll> ump;
+ll getrn()
+{
+    ll x;
+    while(ump[x=rng()]==1);
+    ump[x]=1;
+    return x;
+}
+struct vert
+{
+    ll sum = 0,val = 0;
+    vert (ll _v=0){
+        val = sum = _v;
+    }
+    void reset()
+    {
+        sum = val;
+    }
+    void comb(vert l,vert r)
+    {
+        sum = l.sum+r.sum;
+    }
+};
+class item
+{
+public:
+    ll sz,prior;
+    vert val;
+    item *l,*r;
+    item(vert t=vert(),ll c=1,ll _p=getrn(),item* _l=NULL,item* _r=NULL):val(t),sz(c),prior(_p),l(_l),r(_r){}
+};
+typedef item* pitem;
+class Treap
+{
+public:
+    pitem root;
+    Treap():root(NULL){}
+    ll get_sz(pitem t)
+    {
+        if(t)return t->sz;
+        return 0;
+    }
+    void upd_sz(pitem t)
+    {
+        if(t)t->sz=get_sz(t->l)+get_sz(t->r)+1;
+    }
+    void reset(pitem t)
+    {
+        if(t)t->val.reset();
+    }
+    void combine(pitem &t,pitem l,pitem r)
+    {
+        if(!l||!r)t=(l?l:r);
+        else t->val.comb(l->val,r->val);
+    }
+    void proc(pitem t)
+    {
+        if(!t)return;
+        reset(t);
+        combine(t,t->l,t);
+        combine(t,t,t->r);
+    }
+    void split(pitem t,pitem &l,pitem &r,int key,int add=0)
+    {
+        if(!t)return void(l=r=NULL);
+        int cur_pos=add+get_sz(t->l);
+        if(key<=cur_pos)split(t->l,l,t->l,key,add),r=t;
+        else split(t->r,t->r,r,key,cur_pos+1),l=t;
+        upd_sz(t);
+        proc(t);
+    }
+    void merge(pitem &t,pitem l,pitem r)
+    {
+        if(!l||!r)return void(t=(l?l:r));
+        if(l->prior<r->prior)merge(r->l,l,r->l),t=r;
+        else merge(l->r,l->r,r),t=l;
+        upd_sz(t);
+        proc(t);
+    }
+    void build(int ss,int sf,pitem &t)
+    {
+        if(ss>sf)return;
+        if(ss==sf)return void(t=new item(vert(ara[ss])));
+        int mid=(ss+sf)/2;
+        t=new item(vert(ara[mid]));
+        pitem a=NULL,b=NULL;
+        build(ss,mid-1,a);
+        build(mid+1,sf,b);
+        merge(a,a,t);
+        merge(t,a,b);
+    }
+    void build(int ss,int sf){build(ss,sf,root);}
+    void insert(vert v,int pos)
+    {
+        if(!root)return void(root=new item(v));
+        pitem l,r;
+        l=r=NULL;
+        split(root,l,r,pos-1);
+        merge(l,l,new item(v));
+        merge(root,l,r);
+    }
+    void erase(int pos,pitem &t,int add=0)
+    {
+        if(!t)return;
+        if(get_sz(t->l)+add+1==pos)merge(t,t->l,t->r),proc(t);
+        else if(get_sz(t->l)+add>=pos)erase(pos,t->l,add);
+        else erase(pos,t->r,add+get_sz(t->l)+1);
+    }
+    void erase(int pos){
+        if(!root)return;
+        pitem l=NULL,r=NULL;
+        split(root,l,r,pos);
+        pitem w=NULL;
+        split(l,l,w,pos-1);
+        delete w;
+        merge(root,l,r);
+    }
+    void replace(int pos,vert v)
+    {
+        if(!root)return;
+        pitem l=NULL,r=NULL;
+        split(root,l,r,pos);
+        pitem w=NULL;
+        split(l,l,w,pos-1);
+        w->val=v;
+        merge(l,l,w);
+        merge(root,l,r);
+    }
+    vert find(int pos,pitem t,int add=0)
+    {
+        if(get_sz(t->l)+add+1==pos)return t->val;
+        else if(get_sz(t->l)+add>=pos)return find(pos,t->l,add);
+        else return find(pos,t->r,add+get_sz(t->l)+1);
+    }
+    vert find(int pos){return find(pos,root);}
+    vert query(int l,int r,pitem t)
+    {
+        pitem L=NULL,mid=NULL,R=NULL;
+        split(t,L,mid,l-1);
+        split(mid,t,R,r-l+1);
+        vert ans=t->val;
+        merge(mid,L,t);
+        merge(t,mid,R);
+        return ans;
+    }
+    vert query(int l,int r){return query(l,r,root);}
+    size_t size(){
+        return get_sz(root);
+    }
+};
+
+
+
+
+
+
+
+
+
+//Particular problem
 unsigned int ara[asz];
 unsigned int C[20][20];
 unsigned int pwr[asz][20];
