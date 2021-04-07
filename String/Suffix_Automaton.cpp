43d3dcc11d9636a8f9c9a2ca266dc57344c29f2f
Revenant27 <60836699+Revenant27@users.noreply.github.com>
2021-04-08 02:22:52 +0600

    Update Suffix_Automaton.cpp

--- String/Suffix_Automaton.cpp
+++ String/Suffix_Automaton.cpp
@@ -1,3 +1,54 @@
+
+struct vertex{
+	int link,len;
+	int next[26];
+	vertex(){
+		link = -1;
+		len = 0;
+		memset(next,-1,sizeof next);
+	}
+};
+
+vertex sa[N*2];
+
+int last = 0,sz = 1;
+void add_char(char c){
+	c-='a';
+	int cur = sz++;
+	sa[cur].len = sa[last].len+1;
+	int u = last;
+	while(u!=-1&&sa[u].next[c]==-1)sa[u].next[c] = cur, u = sa[u].link;
+	if(u==-1)
+		sa[cur].link = 0;
+	else{
+		int v = sa[u].next[c];
+		if(sa[u].len+1 == sa[v].len)
+			sa[cur].link = v;
+		else{
+			int nw = sz++;
+			sa[nw].link = sa[v].link;
+			sa[nw].len = sa[u].len+1;
+			memcpy(sa[nw].next,sa[v].next,sizeof sa[v].next);
+			while(u!=-1&&sa[u].next[c]==v)
+				sa[u].next[c] = nw, u = sa[u].link;
+			sa[cur].link = sa[v].link = nw;
+		}
+	}
+	last = cur;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 struct vertex{
     int link,len;
     map<char,int> next;
